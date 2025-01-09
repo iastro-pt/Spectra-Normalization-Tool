c870c64058ea4c5f26826d195d8c0b2e0b4eab88
Kamuish <andremiguel952@gmail.com>
2025-01-09 19:08:08 +0000

    Update main code loop

--- src/SNT/snt.py
+++ src/SNT/snt.py
@@ -1,11 +1,18 @@
+import json
+import multiprocessing
+from collections import defaultdict
+from functools import partial
 from pathlib import Path
-from typing import Optional
+from typing import Any, Optional
 
 import numpy as np
 import scipy.constants as constant
 from loguru import logger
 from matplotlib import pyplot as plt
 from scipy.signal import find_peaks, savgol_filter
+
+from SNT.utils.json_compatible import json_ready_converter
+from SNT.utils.SNT_configs import construct_SNT_configs
 
 from .proj_functions import a_shape, continuum, p_map, smooth
 
@@ -15,45 +22,117 @@ def normalize_spectra(
     spectra,
     header,
     output_path,
-    config,
+    user_config: dict[str, Any] | None = None,
     FWHM_KW: Optional[str] = None,
     FWHM_override: Optional[float] = None,  # noqa: N803
     store_to_disk: bool = True,
 ):
+    wavelengths = np.asarray(wavelengths)
+    spectra = np.asarray(spectra)
+    if spectra.ndim == 1:
+        wavelengths = wavelengths[np.newaxis, :]
+        spectra = spectra[np.newaxis, :]
 
-    min_lambda = min(wavelengths)
     if FWHM_KW is None:
-        FWHM_KW = 'HIERARCH ESO QC CCF FWHM'
+        # Default to ESO pipeline
+        FWHM_KW = "HIERARCH ESO QC CCF FWHM"
 
-    if FWHM_override is not None:
-        FWHM = FWHM_override
-    else:
-        FWHM = header[FWHM_KW]  # FWHM in Km/s
+    FWHM = header[FWHM_KW] if FWHM_override is None else FWHM_override  # FWHM in Km/s
 
-    FWHM_WL = min_lambda * (FWHM / (constant.c / 1000))  # FWHM in A
     # FWHM_WL=0.1 in case the spectre doesn't include information about FWHM
 
-    # ---------------------------------
-    # Parameters (change values in config.py)
-    # ---------------------------------
-    remove_n_first = config["remove_n_first"]  # removes the first n points in the spectra (only if first points are not useful!) otherwise set to 0
-    radius_min = config["radius_min"]  # min alpha shape radius
-    radius_max = config["radius_max"]  # max alpha shape radius (should be at least the size of the largest gap)
-    max_vicinity = config["max_vicinity"]  # required number of values between adjacent maxima
-    global_stretch = config["stretching"]  # normalization parameter
-    use_pmap = config["use_RIC"]  # use the RIC to avoid large "dips" in the continuum (see documentation)
-    interp = config["interp"]  # interpolation type
-    use_denoise = config["use_denoise"]  # for noisy spectra use the average of the flux value around the maximum
-    usefilter = config["usefilter"]  # use savgol filter to smooth spectra
-    nu = config["nu"]  # exponent of the computed penalty (see documentation)
-    niter_peaks_remove = config["niter_peaks_remove"]  # number of iterations to remove sharpest peaks before interpolation
-    denoising_distance = config["denoising_distance"]  # number of points to calculate the average around a maximum if use_denoise is True, useful for noisy spectra
+    config = construct_SNT_configs(user_configs=user_config)
 
     # ---------------------------------
     logger.debug("Running...")
     # -----------Smoothing------------------------------------------
-    spectra_clip = spectra[remove_n_first:]  # removes first n points
+    continuum_values = np.zeros_like(wavelengths)
+
+    byproducts = defaultdict(list)
+    if config["parallel_orders"]:
+        ff = partial(normalize_row, FWHM=FWHM, config=config)
+        with multiprocessing.Pool(config["Ncores"]) as p:
+            out = p.starmap(ff, zip(wavelengths, spectra))
+        for index, entry in enumerate(out):
+            continuum_values[index] = entry[0]
+            for key, value in entry[1].items():
+                byproducts[key].append(value)
+            byproducts["order_index"].append(index)
+    else:
+        for row_index, row in enumerate(spectra):
+            cont, fit_metrics = normalize_row(wavelengths[row_index], row, FWHM, config=config)
+            for key, value in fit_metrics.items():
+                byproducts[key].append(value)
+            byproducts["order_index"].append(row_index)
+            continuum_values[row_index] = cont
+    # export results to csv
+
+    if not isinstance(output_path, Path):
+        output_path = Path(output_path)
+    output_path /= "SNT_data"
+
+    if store_to_disk:
+        output_path.mkdir(exist_ok=True)
+
+        logger.info(f"Data storage folder set to {output_path}")
+        logger.info("Saving text files")
+
+        with open(output_path / "anchors.csv", mode="w") as tow:
+            json.dump(fp=tow, obj=json_ready_converter(byproducts))
+
+        array1 = wavelengths.T
+        array2 = continuum_values.T
+        merged_array = np.empty((array1.shape[0], array1.shape[1] + array2.shape[1]), dtype=array1.dtype)
+
+        merged_array[:, ::2] = array1
+        merged_array[:, 1::2] = array2
+
+        np.savetxt(fname=output_path / "continuum.txt", X=merged_array, delimiter=",", header="wave, flux")
+        if config["run_plot_generation"]:
+            logger.info("Generating plots")
+            fig, ax = plt.subplots(2, sharex=True)
+            # To account for the fact that everything will be a list of lists
+            for a, b in zip(byproducts["max_pos"], byproducts["max_ys"]):
+                ax[0].scatter(a, b, color="g", s=15)
+            for a, b in zip(byproducts["anchors_x"], byproducts["anchors_y"]):
+                ax[0].scatter(a, b, color="r", s=20)
+            for row_index in range(wavelengths.shape[0]):
+                ax[0].plot(wavelengths[row_index], spectra[row_index])
+                ax[0].plot(wavelengths[row_index], continuum_values[row_index], color="r")
+            ax[0].set_ylabel("flux")
+
+            for a, b in zip(byproducts["step_x"], byproducts["step_y"]):
+                ax[1].plot(a, b, color="black")
+            ax[1].set_xlabel("wavelengths")
+            ax[1].set_ylabel("RIC")
+            fig.savefig(output_path / "continuum_plot.png", dpi=600)
+            # ----------------------------------
+    else:
+        logger.warning("Disabled disk storage of data products")
+
+    return continuum_values
+
+
+def normalize_row(wavelengths, spectra, FWHM, config):
+    remove_n_first = config["remove_n_first"]
+    radius_min = config["radius_min"]
+    radius_max = config["radius_max"]
+    max_vicinity = config["max_vicinity"]
+    global_stretch = config["stretching"]
+    use_pmap = config["use_RIC"]
+    interp = config["interp"]
+    use_denoise = config["use_denoise"]
+    usefilter = config["usefilter"]
+    nu = config["nu"]
+    niter_peaks_remove = config["niter_peaks_remove"]
+    denoising_distance = config["denoising_distance"]
+
     wavelengths_clip = wavelengths[remove_n_first:]
+    spectra_clip = spectra[remove_n_first:]
+
+    min_lambda = np.min(wavelengths_clip)
+    FWHM_WL = min_lambda * (FWHM / (constant.c / 1000))  # FWHM in A
+
     spectra_clip, wavelengths_clip = smooth.rolling_sigma_clip(spectra_clip, wavelengths_clip, 20)  # sigma clip twice
     spectra_clip, wavelengths_clip = smooth.rolling_sigma_clip(spectra_clip, wavelengths_clip, 20)
 
@@ -63,25 +142,29 @@ def normalize_spectra(
     s1 = p_map.rolling_max(spectra_clip, wavelengths_clip, FWHM_WL * 40)
     s2 = p_map.rolling_max(spectra_clip, wavelengths_clip, FWHM_WL * 40 * 10)
 
-    # ys1 = s1(wavelengths_clip)
-    # ys2 = s2(wavelengths_clip)
-
     ps = p_map.penalty(s1, s2, wavelengths_clip)
-    if radius_max < 4:
-        step = 1
-    else:
-        step = radius_max / 4
+    step = 1 if radius_max < 4 else radius_max / 4
     step_y, step_x = p_map.step_transform(ps, wavelengths_clip, step)
 
     # ----------Alpha shape maxima selection---------------------
 
     max_index, peaks = find_peaks(spectra_clip, height=0, threshold=None, distance=max_vicinity)
     wavelengths_a = np.array(wavelengths_clip)
-    max_ys = peaks['peak_heights']
+    max_ys = peaks["peak_heights"]
     max_pos = wavelengths_a[max_index]
     anchors_x, anchors_y, anchors_idx = a_shape.anchors(
-        max_index, spectra_clip, wavelengths_clip, step_y, step_x, min_lambda, radius_min, radius_max, nu, use_pmap, global_stretch
-        )
+        max_index,
+        spectra_clip,
+        wavelengths_clip,
+        step_y,
+        step_x,
+        min_lambda,
+        radius_min,
+        radius_max,
+        nu,
+        use_pmap,
+        global_stretch,
+    )
 
     # ------------Outlier removal--------------------------------
 
@@ -93,49 +176,14 @@ def normalize_spectra(
     if use_denoise:
         smooth.denoise(anchors_y, anchors_idx, spectra_clip, denoising_distance)
 
-    if interp == "cubic":
-        fx = continuum.interpolate(anchors_x, anchors_y, "cubic")
-    elif interp == "linear":
-        fx = continuum.interpolate(anchors_x, anchors_y, "linear")
-
-    y_inter = fx(wavelengths)
-
-    # export results to csv
-
-    logger.debug(f"Number of selected maxima: {len(anchors_y)}")
-    if not isinstance(output_path, Path):
-        output_path = Path(output_path)
-    output_path /= "SNT_data"
-
-    if store_to_disk:
-        output_path.mkdir(exist_ok=True)
-
-        logger.info(f"Data storage folder set to {output_path}")
-        logger.info("Saving text files")
-        np.savetxt(
-            fname=output_path/"anchors.csv",
-            X=np.c_[anchors_x, anchors_y], delimiter=",", header="anchorsX, anchorsY"
-            )
-
-        np.savetxt(
-            fname=output_path/"continuum.txt",
-            X=np.c_[wavelengths, y_inter], delimiter=",", header="wave, flux"
-            )
-        logger.info("Generating plots")
-        # plot
-        fig, ax = plt.subplots(2, sharex=True)
-        ax[0].plot(wavelengths, spectra)
-        ax[0].scatter(max_pos, max_ys, color='g', s=15)
-        ax[0].scatter(anchors_x, anchors_y, color='r', s=20)
-        ax[0].plot(wavelengths, y_inter, color='r')
-        ax[0].set_ylabel("flux")
-        ax[1].plot(wavelengths_clip, ps)
-        ax[1].plot(step_x, step_y, color='black')
-        ax[1].set_xlabel("wavelengths")
-        ax[1].set_ylabel("RIC")
-        fig.savefig(output_path / "continuum_plot.png", dpi = 600)
-        # ----------------------------------
-    else:
-        logger.warning("Disabled disk storage of data products")
-
-    return y_inter
+    fx = continuum.interpolate(anchors_x, anchors_y, interp_type=interp)
+    fit_metrics = {
+        "anchors_x": anchors_x,
+        "anchors_y": anchors_y,
+        "max_pos": max_pos,
+        "max_ys": max_ys,
+        "step_y": step_y,
+        "step_x": step_x,
+        "ps": ps,
+    }
+    return fx(wavelengths), fit_metrics
